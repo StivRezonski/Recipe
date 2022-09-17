1af8fd372e674ff521cc223aceb9451f14c53920
Aleksandar Kovac <alexkovach@hotmail.com>
2022-09-17 15:49:53 +0200

    connect app with firebase backend

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,22 +11,24 @@ export class RecipeService {
 
   recipeChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
-      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]),
-    new Recipe('Big Fat Burger',
-      'What else you need to say?',
-      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Buns', 2)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('Tasty Schnitzel',
+  //     'A super-tasty Schnitzel - just awesome!',
+  //     'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]),
+  //   new Recipe('Big Fat Burger',
+  //     'What else you need to say?',
+  //     'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Buns', 2)
+  //     ])
+  // ];
+
+  private recipes: Recipe[] = [];
 
   getRecipes() {
     return this.recipes.slice();
@@ -57,4 +59,9 @@ export class RecipeService {
   }
 
   constructor(private shoppingListService: ShoppingListService) { }
+
+  setRecipe(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
+  }
 }
