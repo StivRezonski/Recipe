2c28d9a28db11d8c309f08959cf566d00cad7abc
Aleksandar Kovac <alexkovach@hotmail.com>
2022-10-16 17:54:48 +0200

    implement core, auth, shared, recepies and shopping modules

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,23 +1,14 @@
 import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appDropdown]',
-  exportAs: 'appDropdown'
+  selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  private wasInside = false;
-  @HostBinding('class.show') isOpen = false;
+
+  @HostBinding('class.open') isOpen = false;
 
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
-    this.wasInside = true;
   }
-
-    @HostListener('document:click') clickout() {
-    if(!this.wasInside) {
-      this.isOpen = false;
-    }
-    this.wasInside = false;
-    }
 
 }
