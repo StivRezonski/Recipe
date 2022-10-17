c0893267cda67ae3800a3f9cc039aa1857f03737
Stivrezonski <alexkovach@hotmail.com>
2022-10-17 14:14:59 +0200

    implement lazy loading modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' }
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(x => x.RecipesModule)},
+  { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(x => x.ShoppingListModule)},
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(x => x.AuthModule)},
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -13,7 +13,7 @@ import { SharedModule } from '../shared/shared.module';
     SharedModule,
     FormsModule,
     RouterModule.forChild([
-    { path: 'shopping-list', component: ShoppingListComponent },
+    { path: '', component: ShoppingListComponent },
   ])]
 })
 
